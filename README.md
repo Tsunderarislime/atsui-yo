add9b172d80a13c9921083f6c9b09b7b1381cac1
Kyle Mui <kyle_mui@sfu.ca>
2023-07-14 23:52:13 -0700

    Updated main.py, bot can go online now

--- README.md
+++ README.md
@@ -21,4 +21,4 @@ This is a Discord bot that sends messages daily. It sends out the weather (temp 
 - Choose the temperature threshold to post the audio clip (TBD)
 
 ## Can I add this bot to my Discord server?
-Currently unsure whether to make this a public bot, if people really want it then sure I guess. It's currently just a project for fun, but time will tell if this will become a "proper" bot that will see continuous development.
+If you have a way to host it, then sure go ahead. You will have to modify *token.txt* beforehand.
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord as ds
 
-#Replace this
+#Load in the token found in token.txt (MODIFY THE FILE SO THAT IT CONTAINS YOUR OWN BOT TOKEN)
 with open('token.txt') as f:
     token = f.readline()
     f.close()
 
-client = ds.Client()
+#Initialize the Discord client object
+intents = ds.Intents.default()
+intents.message_content = True
 
+client = ds.Client(intents=intents)
+
+#Client event listeners
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -19,4 +24,6 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+
+#All is good, run the bot
 client.run(token)
