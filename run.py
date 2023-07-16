cb2ce48e3fa14f5df609a5a84e26f6422d7f31bb
Kyle Mui <kyle_mui@sfu.ca>
2023-07-16 00:42:43 -0700

    Modified loop exit to work with Linux

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-#Exit code 1 means start/restart
+#Any exit code that isn't 0 means start/restart the bot
 exit_code = 1
 
-while exit_code == 1:
+while exit_code != 0:
     #Run the bot like this so that updates to the config can be seen live
     exit_code = os.system('python main.py')
     time.sleep(3)
